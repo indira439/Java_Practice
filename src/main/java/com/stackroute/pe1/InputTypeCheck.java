c34a227a648bc1ba43a3ae08788d2fa0c08fdb1d
indu <indira.indukuri@cgi.com>
2019-07-12 11:23:42 +0530

    InputTypeCheck test case completed

--- src/main/java/com/stackroute/pe1/InputTypeCheck.java
+++ src/main/java/com/stackroute/pe1/InputTypeCheck.java
@@ -4,25 +4,23 @@ import java.util.Scanner;
 
 public class InputTypeCheck {
 
-    public static void main(String[] args) {
+        public String inputTypeChecker(char inputCharacter){
+            String characterType = "";
+            if (Character.isAlphabetic(inputCharacter)) {
+                if (Character.isUpperCase(inputCharacter)) {
+                    characterType = "Capital letter";
+                } else if (Character.isLowerCase(inputCharacter)) {
+                    characterType = "Lowercase letter";
+                }
+            }
+            else if (Character.isDigit(inputCharacter)){
+                characterType = "Digit";
+            }
+            else {
+                characterType = "Special symbol";
+            }
 
-        char a;
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the input string :");
-        a = input.next().charAt(0);
-
-        if (a >= 'a' && a <= 'z') {
-            System.out.println("Entered character is a Small letter");
-        } else if (a >= 'A' && a <= 'Z') {
-            System.out.println("Entered character is a Capital letter");
-
-        } else if (a >= '0' && a <= '9') {
-            System.out.println("Entered character is a digit");
-        } else {
-            System.out.println("Entered character is a special symbol");
+            return characterType;
         }
-    }
 }
 
