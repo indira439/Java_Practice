a8522676f323f5258ebc2c58d1dde3e54319a812
indu <indira.indukuri@cgi.com>
2019-07-12 11:39:37 +0530

    Palindrome not completed. have to modify

--- src/main/java/com/stackroute/pe1/Palindrome.java
+++ src/main/java/com/stackroute/pe1/Palindrome.java
@@ -6,27 +6,16 @@ import java.lang.*;
 public class Palindrome  {
     public String palindromeSum(long inputNum){
         String returnResult = "Not a palindrome";
-        if (isPalindrome(inputNum)) {
             if ( (isSumOfEvenNum(inputNum) > 25) ) {
                 returnResult = "Palindrome and greater than 25";
             }
             else {
-                returnResult = "Palindrome and lesser than 25";
+                returnResult = "Palindrome and smaller than 25";
             }
         }
-        return returnResult;
     }
 
-    private boolean isPalindrome(long number){
-        String str = "";
-        String stringNum = String.valueOf(number);
-        boolean boo = false;
-        for (int i = stringNum.length() -1; i >=0; i--){
-            str = str.concat( String.valueOf(stringNum.charAt(i)) );
-        }
-        if (str.equals(stringNum)) boo = true;
-        return boo;
-    }
+
 
     private int isSumOfEvenNum(long number){
         int sumOfEvenNum = 0;
