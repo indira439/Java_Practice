8006d55bf2b27a5af54cac3bde43fc89c144e92f
indu <indira.indukuri@cgi.com>
2019-07-23 19:34:08 +0530

    VowelOrConsonant test case and logic completed

--- src/main/java/com/stackroute/pe1/VowelOrConsonant.java
+++ src/main/java/com/stackroute/pe1/VowelOrConsonant.java
@@ -14,11 +14,11 @@ public class VowelOrConsonant {
         public String[] vowelOrConsonantChecker(String string) {
             String[] returnResult = new String[string.length()];
             for(int i = 0; i <= string.length() - 1; i++){
-                char ch = string.charAt(i);
-                if(Character.isAlphabetic(ch)){
-                    if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E'
-                            || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O'
-                            || ch == 'u' || ch == 'U'){
+                char character = string.charAt(i);
+                if(Character.isAlphabetic(character)){
+                    if (character == 'a' || character == 'A' || character == 'e' || character == 'E'
+                            || character == 'i' || character == 'I' || character == 'o' || character == 'O'
+                            || character == 'u' || character == 'U'){
                         returnResult[i] = "Vowel";
                     }
                     else {
--- src/test/java/com/stackroute/pe1/VowelOrConsonantTest.java
+++ src/test/java/com/stackroute/pe1/VowelOrConsonantTest.java
@@ -8,43 +8,69 @@ import static org.junit.Assert.*;
 
 public class VowelOrConsonantTest {
 
-        VowelOrConsonant vowelOrConsonant;
+    VowelOrConsonant vowelOrConsonant;
 
-        @Before
-        public void setUp() {
-            this.vowelOrConsonant = new VowelOrConsonant();
-        }
-
-        @After
-        public void teardown() {
-        //arrange
-        System.out.println("Inside After");
-        vowelOrConsonant= null;
-        }
-
-
-        @Test
-        public void givenStringShouldReturnNotALetter() {
-            //act
-            String actualResult[] = this.vowelOrConsonant.vowelOrConsonantChecker("9");
-            //assert
-            assertEquals("Not a letter", actualResult[0]);
-        }
-
-        @Test(expected = NullPointerException.class)
-        public void givenStringShouldReturnNullPointerException() {
-            //act
-            this.vowelOrConsonant.vowelOrConsonantChecker(null);
-            //assert
-        }
-
-
-        @Test
-        public void givenStringShouldReturnAnArrayWithAllPossibleResult() {
-            //act
-            String[] actualResult = this.vowelOrConsonant.vowelOrConsonantChecker("is9");
-            //assert
-            String[] expected = {"Vowel", "Consonant", "Not a letter"};
-            assertArrayEquals(expected, actualResult);
-        }
+    @Before
+    public void setUp() {
+        this.vowelOrConsonant = new VowelOrConsonant();
     }
+
+    @After
+    public void teardown() {
+        //arrange
+        vowelOrConsonant = null;
+    }
+
+
+    @Test
+    public void givenDigitShouldReturnErrorMessage() {
+        //act
+        String actualResult[] = this.vowelOrConsonant.vowelOrConsonantChecker("9");
+        //assert
+        assertEquals("Not a letter", actualResult[0]);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void givenNullShouldReturnNullPointerException() {
+        //act
+        this.vowelOrConsonant.vowelOrConsonantChecker(null);
+        //assert
+    }
+
+
+    @Test
+    public void givenStringShouldReturnAnArrayWithTypeOfCharacter() {
+        //act
+        String[] actualResult = this.vowelOrConsonant.vowelOrConsonantChecker("is9");
+        //assert
+        String[] expected = {"Vowel", "Consonant", "Not a letter"};
+        assertArrayEquals(expected, actualResult);
+    }
+
+    @Test
+    public void givenVowelCharacterShouldReturnVowel() {
+        //act
+        String[] actualResult = this.vowelOrConsonant.vowelOrConsonantChecker("I");
+        //assert
+        String[] expected = {"Vowel"};
+        assertArrayEquals(expected, actualResult);
+    }
+
+    @Test
+    public void givenConsonantCharacterShouldReturnConsonant() {
+        //act
+        String[] actualResult = this.vowelOrConsonant.vowelOrConsonantChecker("Z");
+        //assert
+        String[] expected = {"Consonant"};
+        assertArrayEquals(expected, actualResult);
+    }
+
+    @Test
+    public void givenDigitShouldReturnNotALetter() {
+        //act
+        String[] actualResult = this.vowelOrConsonant.vowelOrConsonantChecker("9");
+        //assert
+        String[] expected = {"Not a letter"};
+        assertArrayEquals(expected, actualResult);
+    }
+}
